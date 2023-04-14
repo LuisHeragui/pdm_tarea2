c48a46e94e1d041a7c09b377004205eb22586245
Luis Alberto Hernández Aguilar <luis_hernandez@ciencias.unam.mx>
2023-04-14 01:35:13 -0600

    Implementando primera interfaz basica

---
+++ app/src/main/java/com/example/tarea2/HolaTodos.java
@@ -0,0 +1,16 @@
+package com.example.tarea2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class HolaTodos extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main_layout);
+    }
+}
