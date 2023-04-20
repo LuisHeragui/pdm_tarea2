b8d4641086398f4ee0045dc6396bdf7924c4c7d1
Luis Alberto Hernández Aguilar <luis_hernandez@ciencias.unam.mx>
2023-04-19 20:54:27 -0600

    Finalizando pantallas

--- app/src/main/java/com/example/tarea2/MainActivity.java
+++ app/src/main/java/com/example/tarea2/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.tarea2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
@@ -9,10 +11,20 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //Pantalla 1
-        setContentView(R.layout.main_layout);
+        boolean verPantalla1 = false;
 
-        //Pantalla 2
-        //setContentView(R.layout.formulario_pagina_1);
+        if (verPantalla1) {
+            // Pantalla 1
+            setContentView(R.layout.main_layout);
+
+            Spinner spinner = findViewById(R.id.hobby);
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                    R.array.hobbies_array, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(adapter);
+        } else {
+            // Pantalla 2
+            setContentView(R.layout.formulario_pagina_1);
+        }
     }
 }
